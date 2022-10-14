b36de5c753f256c062a7d3f15458a835598e85b1
Bence Szilagyi <szilagyi.bence00@gmail.com>
2022-10-14 11:18:07 +0100

    put checks back in Order.java

--- src/main/java/uk/ac/ed/inf/Order.java
+++ src/main/java/uk/ac/ed/inf/Order.java
@@ -3,6 +3,7 @@ package uk.ac.ed.inf;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.IOException;
 import java.util.*;
 
 public class Order {
@@ -149,6 +150,65 @@ public class Order {
                     runningList.addAll(newMenuArray);
                     return runningList;
                 });
+    }
+
+
+    // stuff that'll probably be useful later
+    private boolean checkCardNumber() {
+        boolean checkOnlyDigits = this.creditCardNumber.matches("[0-9]+");
+        boolean checkLength = this.creditCardNumber.length() == 16;
+
+        return checkOnlyDigits && checkLength;
+    }
+
+    private boolean checkExpiryDate() {
+        boolean checkLength = this.creditCardExpiry.length() == 5;
+        if (checkLength) {
+            return this.creditCardExpiry.matches("(10|11|12|0[1-9])\\/(2[3-9]|[3-9][0-9])");  // Technically this will become wrong if we change centuries, but for the time being it is safer.
+            // This is a reminder to change it if we get to that. Sorry 2100s developers, and hello from the past :)
+            // No need to check for the edge case where we are in 2023 but before the service's starting date since it starts on January 1st :)
+        } else return false;
+    }
+
+    private boolean checkCVV() {
+        return this.cvv.matches("[0-9]{3}");
+    }
+
+    private boolean checkPizzaCount() {
+        int l = this.orderItems.length;
+        return 0 < l && l < 5;
+    }
+
+    private boolean checkPizzasDefined() throws IOException {
+        // TODO: change address in CW2
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(Constants.DEFAULT_BASE_ADDRESS);
+        try { // Make sure we don't crash if we somehow get a null value in restaurants
+            assert restaurants != null;
+        } catch (AssertionError e) {
+            return false;
+        }
+
+        List<Menu> allMenuItems = getAllMenuItems(restaurants);
+
+        HashSet<String> allMenuNames = new HashSet<>(allMenuItems.stream() // we want a HashSet for fast lookup
+                .map(Menu::getName) // map List<Menu>
+                .toList()); // to List<String>
+
+        return Arrays.stream(this.orderItems)
+                .allMatch(allMenuNames::contains); // ensure every pizza name in the order is present in at least one restaurant's menu
+    }
+
+    private boolean checkTotal() throws IOException {
+        // difficult to remove code duplication and also retain readability due to the try-catch, I think the code is least confusing kept this way
+        // TODO: change address in CW2
+        Restaurant[] restaurants = Restaurant.getRestaurantsFromRestServer(Constants.DEFAULT_BASE_ADDRESS);
+        try { // Make sure we don't crash if we somehow get a null value in restaurants
+            assert restaurants != null;
+        } catch (AssertionError e) {
+            return false;
+        }
+
+        return calcTotal(restaurants, this.orderItems) + 100 == this.priceTotalInPence;
     }
 
 }
